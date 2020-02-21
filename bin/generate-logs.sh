c3302a4f5ee970628073fa243a47b48bc4ab17a3
CJ Salem <cj.salem@fishtech.group>
2020-02-21 11:11:29 -0600

    adjust logging speed

--- bin/generate-logs.sh
+++ bin/generate-logs.sh
@@ -9,4 +9,4 @@ echo "I'm gonna start piping fake logs into ${OUTPUT_FILE}"
 
 sleep 5
 
-docker run -it --rm mingrammer/flog -l -d 0.01 -s 1 | tee -a ${OUTPUT_FILE}
+docker run -it --rm mingrammer/flog -l -d 0.1 -s 1 | tee -a ${OUTPUT_FILE}
