fb38e5472165ec3549cd8ecd30f476b2d89bc88e
specialblend <cj@specialblend.net>
2020-02-20 23:22:58 -0600

    setup log generator

---
+++ bin/generate-logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NOW=$(date +%F-%H-%M-%S)
+OUTPUT_FILE="logs/${NOW}.log"
+
+mkdir -p logs
+
+echo "I'm gonna start piping fake logs into ${OUTPUT_FILE}"
+
+docker run -it --rm mingrammer/flog -l -d 0.5 -s 1 | tee -a ${OUTPUT_FILE}
