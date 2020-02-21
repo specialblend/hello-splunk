7b2c4dd99284397cbc5fae5464e6291200c979a6
CJ Salem <cj.salem@fishtech.group>
2020-02-21 11:07:33 -0600

    speed up logs

--- bin/generate-logs.sh
+++ bin/generate-logs.sh
@@ -7,4 +7,4 @@ mkdir -p logs
 
 echo "I'm gonna start piping fake logs into ${OUTPUT_FILE}"
 
-docker run -it --rm mingrammer/flog -l -d 0.5 -s 1 | tee -a ${OUTPUT_FILE}
+docker run -it --rm mingrammer/flog -l -d 0.01 -s 1 | tee -a ${OUTPUT_FILE}
